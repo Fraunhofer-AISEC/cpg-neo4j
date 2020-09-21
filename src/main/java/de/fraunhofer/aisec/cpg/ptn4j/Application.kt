e5191c7490f4081a3fe1c79d0c88d2aaaa66b9ae
Specht, Tobias <tobias.specht@aisec.fraunhofer.de>
2020-09-21 13:50:29 +0200

    join include path with parent dir of includesFile

--- src/main/java/de/fraunhofer/aisec/cpg/ptn4j/Application.kt
+++ src/main/java/de/fraunhofer/aisec/cpg/ptn4j/Application.kt
@@ -180,13 +180,13 @@ class Application : Callable<Int> {
                 .defaultPasses()
                 .loadIncludes(loadIncludes)
                 .debugParser(true)
-        
 
         includesFile?.let {
             println("Load includes form file: " + it)
+            val baseDir = File(it.toString()).parentFile.toString()
             val inputStream: InputStream = it.inputStream()
             inputStream.bufferedReader().useLines { lines -> lines.forEach { 
-                translationConfiguration.includePath(Paths.get(topLevel.toString(), it).toString())
+                translationConfiguration.includePath(Paths.get(baseDir, it.strip()).toString())
             }}
         }
 
